cb249e920a7dd15e049ba5907eeeb633c901ab14
Ritesh Kumar <Riteshkumar.nitk21@gmail.com>
2024-02-29 17:39:16 +0530

    card bounce motion added

--- reactProjectOne/src/Component/Card.jsx
+++ reactProjectOne/src/Component/Card.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
-import {FaRegFileAlt} from "react-icons/fa";
-import {LuDownload} from "react-icons/lu";
+import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
 import { IoIosClose } from "react-icons/io";
-import { motion } from "framer-motion"
-function Card({data ,refrance}) {
+import { motion } from "framer-motion";
+function Card({ data, refrance }) {
   return (
     <div>
-        <motion.div drag dragConstraints={refrance} className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-9 overflow-hidden'>
-             <FaRegFileAlt/>
-             <p className='text-sm mt-5 font-semibol leading-tights'>{data.desc}</p>
-            <div className='footer absolute bottom-0  w-full left-0'>
-                <div className='flex items-center justify-between  px-8 py-3 mb-3'>
-                    <h5>{data.filesize}</h5>
-                    <span className='w-7 h-7 bg-zinc-500 rounded-full flex items-center justify-center'>
-                        {data.close?<IoIosClose /> : <LuDownload size=".8em" color='#000' />}
-                    </span>
-                </div>
-                {
-                data.tag.isOpen && (
-                <div className={`tag w-full py-4 ${data.tag.tagColor === "green"? "bg-green-600": "bg-sky-600"}  flex items-center justify-center`}>
-                    <h3 className='text-sm  font-semibold  flex justify-center items-center'>{data.tag.tagTitle} </h3>
-                </div>
-                )
-                }
+      <motion.div
+        drag
+        dragConstraints={refrance}
+        whileDrag={{ scale: 1.2 }}
+        dragElastic={0.2}
+        dragTransition={{bounceStiffness:150, BounceDamping:100}}
+        className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-9 overflow-hidden"
+      >
+        <FaRegFileAlt />
+        <p className="text-sm mt-5 font-semibol leading-tights">{data.desc}</p>
+        <div className="footer absolute bottom-0  w-full left-0">
+          <div className="flex items-center justify-between  px-8 py-3 mb-3">
+            <h5>{data.filesize}</h5>
+            <span className="w-7 h-7 bg-zinc-500 rounded-full flex items-center justify-center">
+              {data.close ? (
+                <IoIosClose />
+              ) : (
+                <LuDownload size=".8em" color="#000" />
+              )}
+            </span>
+          </div>
+          {data.tag.isOpen && (
+            <div
+              className={`tag w-full py-4 ${
+                data.tag.tagColor === "green" ? "bg-green-600" : "bg-sky-600"
+              }  flex items-center justify-center`}
+            >
+              <h3 className="text-sm  font-semibold  flex justify-center items-center">
+                {data.tag.tagTitle}{" "}
+              </h3>
             </div>
-        </motion.div>
+          )}
+        </div>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
